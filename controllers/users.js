09ec8f439ce37ee615ce68db2569ae23c6d525de
Riley Johnston <rileydanejohnston@gmail.com>
2022-01-11 09:29:56 -0600

    feat: add custom email function in validateUsers, refactor signup function

--- controllers/users.js
+++ controllers/users.js
@@ -5,13 +5,13 @@ module.exports.signup = (req, res, next) => {
   const { email, password, name } = req.body;
 
   bcrypt.hash(password, 10)
-    .then((hash) => {
-      Users.create({ email, password: hash, name })
-      .then((user) => {
-        res.status(201).send(user);
-      })
+    .then((hash) => Users.create({ email, password: hash, name }))
+    .then((user) =>  {
+      res.status(201).send({ email: user.email, name: user.name })
     })
     .catch((err) => {
+      console.log(`name: ${err.name}`);
+      console.log(`message: ${err.message}`);
       res.send({ err: err.message });
     });
 }
--- middlewares/validateUsers.js
+++ middlewares/validateUsers.js
@@ -1,8 +1,16 @@
 const { celebrate, Joi } = require('celebrate');
+const { isEmail } = require('validator');
+
+const validateEmail = (value, helpers) => {
+  if (isEmail(value)) {
+    return value;
+  }
+  return helpers.error();
+}
 
 module.exports.validateSignup = celebrate({
   body: Joi.object().keys({
-    email: Joi.string().required().min(2).max(30),
+    email: Joi.string().required().min(2).max(30).custom(validateEmail),
     password: Joi.string().required().min(8),
     name: Joi.string().required().min(2).max(30),
   }),
