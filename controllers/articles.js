7d592a91be7deccf4c1a4afd92254775f729c6fa
Riley Johnston <rileydanejohnston@gmail.com>
2022-01-12 18:17:33 -0600

    feat: add basic deleteArticle function

--- controllers/articles.js
+++ controllers/articles.js
@@ -1,4 +1,5 @@
 const Articles = require('../models/article');
+const ErrorManager = require('../errors/ErrorManager');
 
 module.exports.getArticles = (req, res, next) => {
   Articles.find({})
@@ -32,4 +33,24 @@ module.exports.saveArticle = (req, res, next) => {
       owner
     }))
     .catch(next);
+}
+
+module.exports.deleteArticle = (req, res, next) => {
+  const { articleId } = req.params;
+  const { _id } = req.user;
+
+  Articles.findById(articleId).select('+owner')
+    .orFail(new ErrorManager(404, 'Delete article failed. The article was not found.'))
+    .then((article) => {
+      if (article.owner.toString() !== _id) {
+        return Promise.reject(new ErrorManager(403, 'Delete article failed. The article is not yours to delete.'));
+      }
+
+      return Articles.findByIdAndDelete(article._id);
+    })
+    // send only the id of deleted card?
+    // frontend matches id and removes it from saved
+    .then((article) => res.status(200).send({ _id: article._id }))
+    .catch(next);
+
 }
--- routes/articles.js
+++ routes/articles.js
@@ -2,10 +2,12 @@ const router = require('express').Router();
 const {
   saveArticle,
   getArticles,
+  deleteArticle,
 } = require('../controllers/articles');
 const { validateSave } = require('../middlewares/validateArticles');
 
 router.get('/', getArticles);
 router.post('/', validateSave, saveArticle);
+router.delete('/:articleId', deleteArticle);
 
 module.exports = router;
