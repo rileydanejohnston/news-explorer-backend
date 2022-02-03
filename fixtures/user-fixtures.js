2ff37a18a6f3ae74670c9d5f63a1be2d43d8d970
Riley Johnston <rileydanejohnston@gmail.com>
2022-02-03 14:54:48 -0600

    feat: add tests for joi email validation for /signup

--- fixtures/user-fixtures.js
+++ fixtures/user-fixtures.js
@@ -4,6 +4,43 @@ const validSignup = {
   name: 'validEmail'
 };
 
+const badEmailSignup = {
+  email: 'validEmailgmail.com',
+  password: 'superstrongpassword',
+  name: 'validEmail'
+}
+const badPasswordSignup = {
+  email: 'validEmail@gmail.com',
+  password: 'sup',
+  name: 'validEmail'
+}
+
+const badNameSignup = {
+  email: 'validEmail@gmail.com',
+  password: 'superstrongpassword',
+  name: 'val'
+}
+
+const noEmailSignup = {
+  password: 'superstrongpassword',
+  name: 'validEmail'
+}
+const noPasswordSignup = {
+  email: 'validEmail@gmail.com',
+  name: 'validEmail'
+}
+
+const noNameSignup = {
+  email: 'validEmail@gmail.com',
+  password: 'superstrongpassword',
+}
+
 module.exports = {
   validSignup,
+  badEmailSignup,
+  badPasswordSignup,
+  badNameSignup,
+  noEmailSignup,
+  noPasswordSignup,
+  noNameSignup
 }
--- tests/endpoints.test.js
+++ tests/endpoints.test.js
@@ -1,9 +1,17 @@
 const app = require('../app');
 const mongoose = require('mongoose');
 const supertest = require('supertest');
-const { validSignup } = require('../fixtures/user-fixtures');
 const User = require('../models/user');
 const { MONGO_URL } = process.env;
+const {
+  validSignup,
+  badEmailSignup,
+  badPasswordSignup,
+  badNameSignup,
+  noEmailSignup,
+  noPasswordSignup,
+  noNameSignup
+ } = require('../fixtures/user-fixtures');
 
 const request = supertest(app);
 
@@ -22,7 +30,7 @@ describe('test API requests', () => {
     return User.deleteOne({ email: validSignup.email });
   })
 
-  test('successful /signup returns 201 status, email and username', () => {
+  test('valid data to /signup returns 201 status & email/username', () => {
     // make a request for something
     return request.post('/signup').send(validSignup)
       .then((response) => {
@@ -30,6 +38,23 @@ describe('test API requests', () => {
         expect(response.body.email).toBe(validSignup.email);
         expect(response.body.name).toBe(validSignup.name);
       });
+  })
+
+  // TESTING JOI/CELEBRATE VALIDATION
+  test('invalid email to /signup returns 400 status & invalid email message', () => {
+    return request.post('/signup').send(badEmailSignup)
+      .then((response) => {
+        expect(response.status).toBe(400);
+        expect(response.body.validation.body.message).toBe("\"email\" must be a valid email");
+      });
+  })
+
+  test('missing email to /signup returns 400 status & email required message', () => {
+    return request.post('/signup').send(noEmailSignup)
+    .then((response) => {
+      expect(response.status).toBe(400);
+      expect(response.body.validation.body.message).toBe("\"email\" is required");
+    });
   })
 
   /* test('test bad email validation for /signup', () => {
